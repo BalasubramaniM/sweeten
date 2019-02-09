0927d8c938502e1c8345afca99c9c4a61e457aee
Balasubramani M <bala.m@live.in>
2019-02-10 02:34:41 +0530

    Updates

---
+++ precache-manifest.51a54e0a258da955f441bc4e6e702851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0879a55dfeed95b1fb7",
+    "url": "/sweeten/static/js/runtime~main.e0879a55.js"
+  },
+  {
+    "revision": "c27b68b91d5ee952969a",
+    "url": "/sweeten/static/js/main.c27b68b9.chunk.js"
+  },
+  {
+    "revision": "d546d40f76a0a383d5b8",
+    "url": "/sweeten/static/js/1.d546d40f.chunk.js"
+  },
+  {
+    "revision": "c27b68b91d5ee952969a",
+    "url": "/sweeten/static/css/main.d1a1ccf8.chunk.css"
+  },
+  {
+    "revision": "d546d40f76a0a383d5b8",
+    "url": "/sweeten/static/css/1.10935808.chunk.css"
+  },
+  {
+    "revision": "072da1b8784de93f5410314b74651495",
+    "url": "/sweeten/index.html"
+  }
+];
